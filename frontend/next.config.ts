8fc599913dfb3346a7e8ab3723f77dd6202cfb3c
yu8867 <yu8867ki0217@gmail.com>
2025-09-15 13:46:21 +0900

    fix:optimizeCssの無効化

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    optimizeCss: false, // LightningCSS を切って PostCSS を利用
+  },
 };
 
 export default nextConfig;
