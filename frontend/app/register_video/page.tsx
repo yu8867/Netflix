e576655b8492f6bbd50d3794e13ff54c6a0b3486
yu8867 <yu8867ki0217@gmail.com>
2025-09-18 22:16:18 +0900

    add: 動画登録ページにヘッダ追加、loading機能追加

--- frontend/app/register_video/page.tsx
+++ frontend/app/register_video/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
+import Header from "../home/components/Header";
 
 const page = () => {
   const [title, setTitle] = useState<string>("");
@@ -9,7 +10,7 @@ const page = () => {
   const [thumbnailPreview, setThumbnailPreview] = useState<string | null>(null);
   const [videolPreview, setVideolPreview] = useState<string | null>(null);
   const [error, setError] = useState<string>("");
-  const [loading, seTLoading] = useState<bool>(true);
+  const [loading, setLoading] = useState<bool>(false);
 
   const uploadToS3 = async (file: File, type: "imgae" | "video") => {
     const res = await fetch(
@@ -38,6 +39,7 @@ const page = () => {
     if (!video) return setError("動画がアップロードされていません");
 
     setError("");
+    setLoading(true);
 
     const thumbnail_url = await uploadToS3(thumbnail, "image");
     const video_url = await uploadToS3(video, "video");
@@ -55,6 +57,19 @@ const page = () => {
       }),
       credentials: "include",
     });
+
+    if (res.ok) {
+      setLoading(false);
+      setTitle("");
+      setVideo("");
+      setVideolPreview("");
+      setDescription("");
+      setThumbnailPreview("");
+      localStorage.removeItem("title");
+      localStorage.removeItem("description");
+      localStorage.removeItem("thumbnail_preview");
+      localStorage.removeItem("thumbnail");
+    }
   };
 
   const toBase64 = (file: File): Promise<string> =>
@@ -129,128 +144,141 @@ const page = () => {
   }, []);
 
   return (
-    <div className="text-white flex items-center justify-center">
-      <div>
-        <div className="m-8">
-          <h1 className="text-4xl text-center font-bold m-4 mb-12">
-            映画の追加
-          </h1>
-        </div>
-
-        <form onSubmit={handleRegister} className="grid grid-cols-1 gap-6">
-          {/* 動画 */}
-          {videolPreview ? (
-            <div>
-              <div className="flex justify-end">
-                <button
-                  onClick={deleteVideoChanege}
-                  className="border-2 text-white p-1 hover:bg-white cursor-pointer"
-                >
-                  ❌
-                </button>
+    <>
+      <Header className="text-white" />
+      <div className="ml-[6.25%]">
+        <div className="text-white flex items-center justify-center">
+          {loading && (
+            <div className="fixed inset-0 flex items-center justify-center bg-opacity-50 z-50">
+              <div className="bg-white p-6 rounded-lg shadow-lg text-center">
+                <div className="animate-spin h-8 w-8 border-4 border-blue-500 border-t-transparent rounded-full mx-auto mb-4"></div>
+                <p className="text-gray-700">アップロード中です</p>
               </div>
-              <div className="mt-4 flex flex-center justify-center">
-                <video src={videolPreview} controls className="w-220" />
-              </div>
-            </div>
-          ) : (
-            <div className="flex justify-center items-center">
-              <label className="text-white border-2 w-[1300px] h-120 p-2 text-center cursor-pointer hover:bg-gray-800">
-                <div className="h-110 flex flex-col items-center justify-center gap-2">
-                  <img src="./upload.svg" width={48} />
-                  <p>動画をアップロードする</p>
-                </div>
-                <input
-                  type="file"
-                  accept="video/*"
-                  onChange={(e) =>
-                    handleVideoChange(e.target.files?.[0] || null)
-                  }
-                  className="hidden"
-                />
-              </label>
             </div>
           )}
-
-          <div className="flex gap-6">
-            <div className="grid grid-cols-1 gap-6">
-              {/* タイトル */}
-              <div>
-                <input
-                  type="textarea"
-                  placeholder="タイトル"
-                  value={title}
-                  onChange={(e) => handleTitleChange(e)}
-                  className="text-white border-2 w-2xl p-2 hover:bg-gray-800"
-                />
-              </div>
-
-              {/* 詳細 */}
-              <div>
-                <textarea
-                  placeholder="詳細"
-                  value={description}
-                  onChange={(e) => handleDescriptionChange(e)}
-                  className="text-white border-2 w-2xl h-79 p-2 hover:bg-gray-800"
-                />
-              </div>
+          <div>
+            <div className="m-8">
+              <h1 className="text-4xl text-center font-bold m-4 mb-12">
+                映画の追加
+              </h1>
             </div>
 
-            {/* サムネイル画像 */}
-            <div className="">
-              {thumbnailPreview ? (
+            <form onSubmit={handleRegister} className="grid grid-cols-1 gap-6">
+              {/* 動画 */}
+              {videolPreview ? (
                 <div>
                   <div className="flex justify-end">
                     <button
-                      onClick={deleteThumbnailChanege}
+                      onClick={deleteVideoChanege}
                       className="border-2 text-white p-1 hover:bg-white cursor-pointer"
                     >
                       ❌
                     </button>
                   </div>
                   <div className="mt-4 flex flex-center justify-center">
-                    <img
-                      src={thumbnailPreview}
-                      alt="Thumbnail Preview"
-                      width={360}
-                    />
+                    <video src={videolPreview} controls className="w-220" />
                   </div>
                 </div>
               ) : (
                 <div className="flex justify-center items-center">
-                  <label className="border-2 w-2xl h-96 p-2 text-center cursor-pointer hover:bg-gray-800">
-                    <div className="h-92 flex flex-col items-center justify-center gap-2">
-                      <img src="./upload.svg" width={48} className="" />
-                      <p>画像をアップロードする</p>
+                  <label className="text-white border-2 w-[1300px] h-120 p-2 text-center cursor-pointer hover:bg-gray-800">
+                    <div className="h-110 flex flex-col items-center justify-center gap-2">
+                      <img src="./upload.svg" width={48} />
+                      <p>動画をアップロードする</p>
                     </div>
                     <input
                       type="file"
-                      accept="image/*"
+                      accept="video/*"
                       onChange={(e) =>
-                        handleThumbnailChange(e.target.files?.[0] || null)
+                        handleVideoChange(e.target.files?.[0] || null)
                       }
                       className="hidden"
                     />
                   </label>
                 </div>
               )}
-            </div>
-          </div>
 
-          {/*  */}
+              <div className="flex gap-6">
+                <div className="grid grid-cols-1 gap-6">
+                  {/* タイトル */}
+                  <div>
+                    <input
+                      type="textarea"
+                      placeholder="タイトル"
+                      value={title}
+                      onChange={(e) => handleTitleChange(e)}
+                      className="text-white border-2 w-2xl p-2 hover:bg-gray-800"
+                    />
+                  </div>
 
-          <div className="flex flex-col items-center justify-center gap-4">
-            <button
-              type="submit"
-              className="min-w-48 p-1 cursor-pointer border-2 bg-white text-black rounded-xl"
-            >
-              登録する
-            </button>
-            {error ? <div className="text-red-500">{error}</div> : <></>}
+                  {/* 詳細 */}
+                  <div>
+                    <textarea
+                      placeholder="詳細"
+                      value={description}
+                      onChange={(e) => handleDescriptionChange(e)}
+                      className="text-white border-2 w-2xl h-79 p-2 hover:bg-gray-800"
+                    />
+                  </div>
+                </div>
+
+                {/* サムネイル画像 */}
+                <div className="">
+                  {thumbnailPreview ? (
+                    <div>
+                      <div className="flex justify-end">
+                        <button
+                          onClick={deleteThumbnailChanege}
+                          className="border-2 text-white p-1 hover:bg-white cursor-pointer"
+                        >
+                          ❌
+                        </button>
+                      </div>
+                      <div className="mt-4 flex flex-center justify-center">
+                        <img
+                          src={thumbnailPreview}
+                          alt="Thumbnail Preview"
+                          width={360}
+                        />
+                      </div>
+                    </div>
+                  ) : (
+                    <div className="flex justify-center items-center">
+                      <label className="border-2 w-2xl h-96 p-2 text-center cursor-pointer hover:bg-gray-800">
+                        <div className="h-92 flex flex-col items-center justify-center gap-2">
+                          <img src="./upload.svg" width={48} className="" />
+                          <p>画像をアップロードする</p>
+                        </div>
+                        <input
+                          type="file"
+                          accept="image/*"
+                          onChange={(e) =>
+                            handleThumbnailChange(e.target.files?.[0] || null)
+                          }
+                          className="hidden"
+                        />
+                      </label>
+                    </div>
+                  )}
+                </div>
+              </div>
+
+              {/*  */}
+
+              <div className="flex flex-col items-center justify-center gap-4 mb-8">
+                <button
+                  type="submit"
+                  className="min-w-128 p-1 cursor-pointer border-2 bg-white text-black rounded-xl hover:bg-gray-300"
+                >
+                  登録する
+                </button>
+                {error ? <div className="text-red-500">{error}</div> : <></>}
+              </div>
+            </form>
           </div>
-        </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
