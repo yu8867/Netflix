bc3f44a14dd32a5bc395fd0e89dcaf135f30d6f2
yu8867 <yu8867ki0217@gmail.com>
2025-09-18 22:15:03 +0900

    add: 映画追加リンク

--- frontend/app/home/components/Header.tsx
+++ frontend/app/home/components/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="fixed w-1/15 h-screen border-r border-amber-50 bg-black">
+    <div className="fixed w-1/15 h-screen border-r border-amber-50 bg-black text-white">
       <div className="h-1/4"></div>
       <div className="h-1/2 flex flex-col justify-center items-center text-2xl gap-6">
         <Link href="/home">
@@ -12,11 +12,8 @@ const Header = () => {
         <Link href="/home">
           <div className="hover:text-4xl">Search</div>
         </Link>
-        <Link href="/home">
-          <div className="hover:text-4xl">Rank</div>
-        </Link>
-        <Link href="/home">
-          <div className="hover:text-4xl">History</div>
+        <Link href="/register_video">
+          <div className="hover:text-4xl">Add</div>
         </Link>
       </div>
       <div className="h-1/4 flex flex-col justify-center items-center text-xl">
