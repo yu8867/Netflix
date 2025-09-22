947adfe01edfeffdf2db0833c38341a778fca333
yu8867 <yu8867ki0217@gmail.com>
2025-09-22 20:47:04 +0900

    feat:ジャンルスキーマ作成

--- backend/videos/schemas.py
+++ backend/videos/schemas.py
@@ -1,8 +1,16 @@
 from pydantic import BaseModel
-from datetime import datetime
- 
+from typing import List
+
+class Genre(BaseModel):
+    id: int
+    genre: str
+
+class GenreCreate(BaseModel):
+    genre: str
+
 class VideoCreate(BaseModel):
     title:       str
+    genre_ids:    List[int]
     description: str
     thumbnail_url:   str
     video_url:   str
@@ -10,6 +18,7 @@ class VideoCreate(BaseModel):
 class VideoInformation(BaseModel):
     id:          int
     title:       str
+    genres:      List[Genre]
     description: str
     thumbnail_url:   str
     video_url:   str
