0fb1d725c0ac89b10644d9d7c626e5a2358319d2
yu8867 <yu8867ki0217@gmail.com>
2025-09-23 21:31:03 +0900

    feat:最新API

---
+++ frontend/app/home/api/new.ts
@@ -0,0 +1,18 @@
+async function getNewVideo() {
+  const token = localStorage.getItem("access_token");
+  const res = await fetch("http://127.0.0.1:8000/videos", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    credentials: "include",
+  });
+
+  if (res.status == 401) {
+    return [];
+  }
+
+  const data = await res.json();
+  return data;
+}
+
+export default getNewVideo;
